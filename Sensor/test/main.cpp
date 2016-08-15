e57cd595d2f11f1d25c31080cfd5e9ec964dc551
anon <gitdev@web.de>
2016-08-15 14:43:07 +0200

    added test-cases

--- Sensor/test/main.cpp
+++ Sensor/test/main.cpp
@@ -33,4 +33,14 @@ TEST_CASE("readSensor works") {
         REQUIRE(dbi.readStatusOK());
     }
 
+    SECTION("readSensor works for airpressure-sensor") {
+        Sensor Sensor1;
+        Sensor1.setSensorType(new AirPressureSensor());
+        DataBuffer airPressureSensorBuffer = Sensor1.readSensor();
+        REQUIRE(airPressureSensorBuffer.data.size() > 0);
+        REQUIRE(airPressureSensorBuffer.data["Luftdruck_2m"] > -2000);
+        REQUIRE(airPressureSensorBuffer.data["Luftdruck_2m"] <  2000);
+        REQUIRE(dbi.readStatusOK());
+    }
+
 }
